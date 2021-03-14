295be9b41e1a56ddeb0a1f7b7a1bf919bacd2bbd
Brice Fotzo <brice.fotzo@groupe-esigelec.org>
2021-03-14 21:41:08 +0100

    first commit

---
+++ src/linkedin_v2/example.py
@@ -0,0 +1,7 @@
+def say_hello(name):
+    """Says hello to name
+
+    Args:
+        name ([type]): [description]
+    """
+    print("hello ",name)
---
+++ src/linkedin_v2/exceptions.py
@@ -0,0 +1,5 @@
+class ObjectNotFound(Exception):
+    """An exception risen when an expected object is not found"""
+class InvalidOperation(Exception):
+    """An other exception will be raised there"""
+    
